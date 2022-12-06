5b9e64241cde6c443961226a2aefa99cc099b5eb
diorisdash <111952820+diorisdash@users.noreply.github.com>
2022-12-06 13:06:07 -0500

    links.bat
    you can find links to the edgenuity

---
+++ link.bat
@@ -0,0 +1,9 @@
+echo off
+
+echo %https://discord.gg/MJmdvKmjYj%, %https://github.com/diorisdash/assnuity% >> log.txt
+
+echo Current Discord/GitHub is %https://discord.gg/MJmdvKmjYj%                   ,                                %https://github.com/diorisdash/assnuity%.
+
+@pause
+
+exit
